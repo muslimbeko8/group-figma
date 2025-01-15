4c612b7a7ad1c8d64f3490fd3beba130546bfa2f
muslimbeko8 <blakmaxmudov@gmail.com>
2025-01-15 18:52:09 +0500

    fixx

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage:{
+        hero: "url(/image.png)"
+      }
     },
   },
   plugins: [],
