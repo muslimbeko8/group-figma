fbe8508919329363e4ad12e4946f7bd1027b7033
muslimbeko8 <blakmaxmudov@gmail.com>
2025-01-17 13:11:21 +0500

    Resolve merge conflicts

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${creatball.className} antialiased`}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
         <Navbar />
         {children}
         <Footer />
